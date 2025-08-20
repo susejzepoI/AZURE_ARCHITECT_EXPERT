39d868e1cb1eb11ea2d26dc11380cff2354b13b3
Jesus Lopez <jesus_lopez_mm1995@outlook.com>
2025-08-19 23:00:22 -0500

    Update deployment script to parameterize resource group names and fix template file paths

--- 00002-PROJECT-AZURE-POLCY-REMEDIATION-TASKS/main.ps1
+++ 00002-PROJECT-AZURE-POLCY-REMEDIATION-TASKS/main.ps1
@@ -1,20 +1,32 @@
 [CmdletBinding()]
 param (
     [Parameter()]
-    [string]$pSubscriptionName = 'Suscripción de Plataformas de MSDN'
+    [string]$pSubscriptionName = 'Suscripción de Plataformas de MSDN',
+    [Parameter()]
+    [string]$rg1 = '00002-project-enforcing-location-effect-deny',
+    [Parameter()]
+    [string]$rg2 = '00002-project-eforcing-tag-effect-modify'
+
 )
 
-#JLopez-20250508: Deploying the resource group using a bicep template
+#JLopez-20250508: Deploying the resource group using a bicep template.
 az deployment sub create `
-    --name '00002-Deployment' `
+    --name '00002-Deployment-1' `
     --location 'eastus' `
-    --template-file './infra/bicep/resource-group.bicep' `
-    --parameters pName='00002-project-eforcing-tag-effect-modify' pLocation='eastus' `
+    --template-file '../infra/bicep/01.- resource-group/resource-group.bicep' `
+    --parameters pName=$rg1 pLocation='eastus' `
     --subscription $pSubscriptionName
 
 az deployment sub create `
-    --name '00002-Deployment' `
+    --name '00002-Deployment-2' `
     --location 'eastus' `
-    --template-file './infra/bicep/resource-group.bicep' `
-    --parameters pName='00002-project-enforcing-location-effect-deny' pLocation='eastus' `
+    --template-file '../infra/bicep/01.- resource-group/resource-group.bicep' `
+    --parameters pName=$rg2 pLocation='eastus' `
     --subscription $pSubscriptionName
+
+#JLopez-20250819: Deploying the network interface and the virtual network.
+az deployment group create `
+    --name '00002-Deployment-2' `
+    --parameters pAddressPrefix='11.0.0.0/16' pSubnetPrefix='11.0.0.0/24' `
+    --resource-group $rg1 `
+    --template-file '../infra/bicep/02.- network/vnet-1-subnet-1-nic.bicep' 
