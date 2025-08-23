b825fef2f500b6f1a380012b1ef684ca9573a019
Jesus Lopez <jesus_lopez_mm1995@outlook.com>
2025-08-23 15:38:10 -0500

    Add README and Bicep policy assignment template for Azure policy enforcement

---
+++ 00002-PROJECT-AZURE-POLCY-REMEDIATION-TASKS/.policy-assignments/azure-policy-assignments.bicep
@@ -0,0 +1,23 @@
+param pAssignmentName   string
+param pDefinitionID     string
+param pDisplayName      string
+param pDescription      string
+param pMessage          string
+/*
+JLopez-20250823: Assign the policy definition to a scope.
+source: https://learn.microsoft.com/en-us/azure/governance/policy/assign-policy-bicep?tabs=azure-powershell
+*/
+resource policyAssignment 'Microsoft.Authorization/policyAssignments@2024-05-01' = {
+  name: pAssignmentName
+  properties: {
+    displayName: pDisplayName
+    description: pDescription
+    policyDefinitionId: pDefinitionID
+    scope: resourceGroup().id
+    nonComplianceMessages: [
+      {
+        message: pMessage
+      }
+    ]
+  }
+}
--- 00002-PROJECT-AZURE-POLCY-REMEDIATION-TASKS/main.ps1
+++ 00002-PROJECT-AZURE-POLCY-REMEDIATION-TASKS/main.ps1
@@ -1,15 +1,15 @@
 [CmdletBinding()]
 param (
     [Parameter()]
-    [string]$pSubscriptionName = 'Suscripción de Plataformas de MSDN',
-    [Parameter()]
-    [string]$rg1 = '00002-project-enforcing-location-effect-deny',
-    [Parameter()]
-    [string]$rg2 = '00002-project-eforcing-tag-effect-modify'
+    [string]$pSubscriptionName = 'Suscripción de Plataformas de MSDN'
 
 )
-
-#JLopez-20250508: Deploying the resource group using a bicep template.
+#JLopez-20250823: Defining the resource groups to be created.
+$rg1 = '00002-project-effect-modify-eforce-tags'
+$rg2 = '00002-project-effect-deny-enforce-locations'
+$rg3 = '00002-project-effect-deployifnotexists-public-ip'
+ 
+#JLopez-20250508: Deploying the resource group at the subscription level, using a bicep template.
 az deployment sub create `
     --name '00002-Deployment-1' `
     --location 'eastus' `
@@ -19,14 +19,29 @@ az deployment sub create `
 
 az deployment sub create `
     --name '00002-Deployment-2' `
-    --location 'eastus' `
+    --location 'brazilsouth' `
     --template-file '../infra/bicep/01.- resource-group/resource-group.bicep' `
-    --parameters pName=$rg2 pLocation='eastus' `
+    --parameters pName=$rg2 pLocation='brazilsouth' `
+    --subscription $pSubscriptionName
+
+az deployment sub create `
+    --name '00002-Deployment-3' `
+    --location 'westus' `
+    --template-file '../infra/bicep/01.- resource-group/resource-group.bicep' `
+    --parameters pName=$rg2 pLocation='westus' `
+    --subscription $pSubscriptionName
+
+#JLopez-20250823: Deploying the azure policy definition
+az deployment sub create `
+    --name '00002-Deployment-4' `
+    --location 'eastus' `
+    --template-file '../.policy-definitions/azure-policy-enforce-tags.bicep' `
+    --parameters pName='enforce-tags-rg1' pTagName='Project' pTagValue='az305' `
     --subscription $pSubscriptionName
 
 #JLopez-20250819: Deploying the network interface and the virtual network.
-az deployment group create `
-    --name '00002-Deployment-2' `
-    --parameters pAddressPrefix='11.0.0.0/16' pSubnetPrefix='11.0.0.0/24' `
-    --resource-group $rg1 `
-    --template-file '../infra/bicep/02.- network/vnet-1-subnet-1-nic.bicep' 
+# az deployment group create `
+#     --name '00002-Deployment-2' `
+#     --parameters pAddressPrefix='11.0.0.0/16' pSubnetPrefix='11.0.0.0/24' `
+#     --resource-group $rg1 `
+#     --template-file '../infra/bicep/02.- network/vnet-1-subnet-1-nic.bicep' 
