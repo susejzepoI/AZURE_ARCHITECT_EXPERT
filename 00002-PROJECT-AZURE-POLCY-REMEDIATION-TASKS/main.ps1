d6b4a4ec31b90a364152a78a85a4e1ad891a491d
Jesus Lopez <jesus_lopez_mm1995@outlook.com>
2025-11-15 16:23:41 -0500

    Update modified date in main.ps1 to reflect recent changes

--- 00002-PROJECT-AZURE-POLCY-REMEDIATION-TASKS/main.ps1
+++ 00002-PROJECT-AZURE-POLCY-REMEDIATION-TASKS/main.ps1
@@ -1,7 +1,7 @@
 #Author:            Jesus Lopez Mesia
 #Linkedin:          https://www.linkedin.com/in/susejzepol/
 #Created date:      08-05-2025
-#Modified date:     10-27-2025
+#Modified date:     11-15-2025
 
 [CmdletBinding()]
 param (
