ba8d9405fffc650cba41d6300d72e6efe4440ca2
Jesus Lopez <jesus_lopez_mm1995@outlook.com>
2025-12-16 22:22:44 -0500

    Add initial project structure and configuration for My_Hello_World_Api
    - Created project files including .csproj, .sln, and various NuGet-related files.
    - Configured dependencies for Microsoft.AspNetCore.OpenApi and Swashbuckle.AspNetCore.
    - Implemented PowerShell script for Azure resource group deployment.
    - Established project structure for Azure Container Instance deployment.

---
+++ 00001-PROJECT-HELLO-WORLD-AZURE-CONTAINER-INSTANCE/My-Hello-world-api/My_Hello_World_Api/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => "Hello world from my Azure Container Instance!.");
+
+app.MapGet(
+    "/info", () =>
+    {
+        var pEnvironmentVariable = Environment.GetEnvironmentVariable("APP_ENVIRONMENT") ?? "Not Set";
+        return Results.Ok(
+            new
+            {
+                ServiceCollection = "Hello World API",
+                appEnv = pEnvironmentVariable,
+                timeUTC = DateTime.UtcNow
+            }
+        );
+    }
+);
+
+app.Run();
--- 00001-PROJECT-HELLO-WORLD-AZURE-CONTAINER-INSTANCE/main.ps1
+++ 00001-PROJECT-HELLO-WORLD-AZURE-CONTAINER-INSTANCE/main.ps1
@@ -1,7 +1,7 @@
 #Author:            Jesus Lopez Mesia
 #Linkedin:          https://www.linkedin.com/in/susejzepol/
 #Created date:      06-12-2025
-#Modified date:     06-15-2025
+#Modified date:     06-16-2025
 
 [cmdletBinding()]
 param(
