134a939999977ebb0f83f6a98f19994c7130c165
Jesus Lopez <jesus_lopez_mm1995@outlook.com>
2026-08-18 22:48:25 -0500

    updated the main 00003 ps1 file to include the bicep template to deploy a storage account.

--- 00003-Simple-Restaurant-Menu/main.ps1
+++ 00003-Simple-Restaurant-Menu/main.ps1
@@ -15,6 +15,7 @@ param (
 
 $pProject                       = '00003'
 $pResourceGroupName             = "$($pProject)-simple-restaurant-menu-$($EnvironmentValue)"
+$pStorageAccountName            = "$($pProject)sa"
 $pBlobStorageContainerName      = "$($RestaurantName)container"
 $pWebAppName                    = "$($pProject)-$($RestaurantName)-webapp"
 $pServicebusNamespaceName       = "$($pProject)-$($RestaurantName)-sbns"
@@ -24,16 +25,24 @@ Write-Host "Starting deployment for project: $($pProject)" -BackgroundColor Gree
 Write-Host "Using subscription: $($SubscriptionName)" -BackgroundColor Green
 Write-Host "Resource Group: $($pResourceGroupName)" -BackgroundColor Green
 Write-Host "Location: $($pLocation)" -BackgroundColor Green
-Write-Host "Storage Account: $($pBlobStorageContainerName)" -BackgroundColor Green
-Write-Host "Storage Account: $($pBlobStorageContainerName)" -BackgroundColor Green
+Write-Host "Storage Account: $($pStorageAccountName)" -BackgroundColor Green
+Write-Host "Blob storage container: $($pBlobStorageContainerName)" -BackgroundColor Green
 Write-Host "Web App: $($pWebAppName)" -BackgroundColor Green
 Write-Host "Service Bus Namespace: $($pServicebusNamespaceName)" -BackgroundColor Green
 
 
 Write-Host "Deploying the resource group: $($pResourceGroupName)" -BackgroundColor Green
 az deployment sub create `
-    --name $pResourceGroupName `
+    --name 'az-deploy-sub-01-rg' `
     --location $pLocation `
     --template-file '../infra/bicep/01.- resource-group/resource-group.bicep' `
     --parameters pName=$pResourceGroupName pLocation=$pLocation `
     --subscription $SubscriptionName
+
+write-host "Deploying the account storage account $($pStorageAccountName)" -BackgroundColor Green
+az deployment group create `
+    --name 'az-deploy-group-sa-01' `
+    --template-file '../infra/bicep/06.- Azure Blob Storage/simple-storage-account-StorageV2-Standard_LRS-Hot.bicep' `
+    --parameters storageAccountName=$pStorageAccountName storageAccountLocaltion=$pLocation `
+    --resource-group $pResourceGroupName `
+    --debug
