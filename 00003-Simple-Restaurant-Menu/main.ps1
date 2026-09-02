bdaa661be36177f92c927db8c59573ccd130d965
Jesus Lopez Mesia <34073690+susejzepoI@users.noreply.github.com>
2026-09-02 08:53:11 -0500

    00003 Creating of the storage account and containers using bicep (#22)
    * Creating of the 00003-Simple-Restaurant-Menu/ directory into the project.
    
    * added new bicep template to deploy a storage account.
    
    * updated the main 00003 ps1 file to include the bicep template to deploy a storage account.
    
    * Updated the simple-storage-account-StorageV2-Standard_LRS-Hot.bicep to include the blob service into the storage account and to create containers (doesn't work yet).
    
    * Updated these files to replace the subscription name parameter to subscription ID. fix the bicep file to fix the for each containers.
    
    * Updated the bicep template and the 00003 main ps1 script to change the  from an array to an string, the bicep script was change to receive that string and split the values into an array.
    
    * Updated the bicep script to pass through the output names of the containers created.

---
+++ 00003-Simple-Restaurant-Menu/main.ps1
@@ -0,0 +1,55 @@
+#Author:            Jesus Lopez Mesia
+#Linkedin:          https://www.linkedin.com/in/susejzepol/
+#Created date:      13-08-2026
+#Modified date:     01-09-2026
+
+[CmdletBinding()]
+param (
+    [Parameter(mandatory = $true)]
+    [string]$SubscriptionId,
+    [Parameter(mandatory = $true)]
+    [string]$RestaurantName,
+    [string]$EnvironmentTagName    = 'Environment',
+    [string]$EnvironmentValue      = 'Dev'
+)
+
+$pRestaurantName                = $RestaurantName.ToLower()
+$pProject                       = '00003'
+$pResourceGroupName             = "$($pProject)-$($pRestaurantName)-simple-restaurant-menu-$($EnvironmentValue)"
+$pStorageAccountName            = "$($pProject)$($pRestaurantName)sa"
+$pBlobStorageContainerName      = "$($RestaurantName)container"
+$pWebAppName                    = "$($pProject)-$($RestaurantName)-webapp"
+$pServicebusNamespaceName       = "$($pProject)-$($RestaurantName)-sbns"
+$pLocation                      = 'chilecentral'
+$pContainersName                = 'starters,mains,desserts,sides,non-alcoholic-beverages,alcoholic-beverages,backups'
+
+Write-Host "Starting deployment for project: $($pProject)" -BackgroundColor Green
+Write-Host "Using subscription: $($SubscriptionId)" -BackgroundColor Green
+Write-Host "Resource Group: $($pResourceGroupName)" -BackgroundColor Green
+Write-Host "Location: $($pLocation)" -BackgroundColor Green
+Write-Host "Storage Account: $($pStorageAccountName)" -BackgroundColor Green
+Write-Host "Blob storage container: $($pBlobStorageContainerName)" -BackgroundColor Green
+Write-Host "Web App: $($pWebAppName)" -BackgroundColor Green
+Write-Host "Service Bus Namespace: $($pServicebusNamespaceName)" -BackgroundColor Green
+
+Write-Host "Deploying the resource group: $($pResourceGroupName)" -BackgroundColor Green
+az deployment sub create `
+    --name 'az-deploy-sub-01-rg' `
+    --location $pLocation `
+    --template-file '../infra/bicep/01.- resource-group/resource-group.bicep' `
+    --parameters pName=$pResourceGroupName pLocation=$pLocation `
+    --subscription $SubscriptionId
+
+write-host "Deploying the account storage account $($pStorageAccountName)" -BackgroundColor Green
+$ContainersName = (
+    az deployment group create `
+        --name 'az-deploy-group-sa-01' `
+        --template-file '../infra/bicep/06.- Azure Blob Storage/simple-storage-account-StorageV2-Standard_LRS-Hot.bicep' `
+        --parameters storageAccountName=$pStorageAccountName storageAccountLocaltion=$pLocation containerNames=$pContainersName `
+        --resource-group $pResourceGroupName `
+        --query properties.outputs.outContainersCreated.value `
+        --output tsv
+)
+
+Write-Host "The following containers were created in the storage account: $($pStorageAccountName)" -BackgroundColor Yellow
+$ContainersName | ForEach-Object {Write-Host "Container: [$PSItem]" -BackgroundColor Yellow}
---
+++ infra/bicep/06.- Azure Blob Storage/simple-storage-account-StorageV2-Standard_LRS-Hot.bicep
@@ -0,0 +1,34 @@
+param storageAccountName string
+param storageAccountLocaltion string
+param containerNames string
+
+var containersArray = split(containerNames,',')
+
+resource storageAccount 'Microsoft.Storage/storageAccounts@2026-04-01' = {
+  kind: 'StorageV2'
+  location: storageAccountLocaltion
+  name: storageAccountName
+  sku: {
+    name: 'Standard_LRS'
+  }
+  properties: {
+    accessTier: 'Hot'
+    allowBlobPublicAccess: true
+    minimumTlsVersion: 'TLS1_2'
+  }
+}
+
+resource blobService 'Microsoft.Storage/storageAccounts/blobServices@2026-04-01' = {
+  name: 'default'
+  parent: storageAccount
+}
+
+resource containers 'Microsoft.Storage/storageAccounts/blobServices/containers@2026-04-01' = [for containerName in containersArray: {
+  name: containerName
+  parent: blobService
+  properties: {
+    publicAccess: 'None' // Options: 'None', 'Blob', or 'Container'
+  }
+}]
+
+output outContainersCreated array = containersArray
